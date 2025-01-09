53d8432d96594ed94756927c2314ae087aa5080b
FrancescoLuzzi <francescoluzzi99@libero.it>
2025-01-09 02:08:48 +0100

    WIP: profile update

--- app/handlers/user.go
+++ app/handlers/user.go
@@ -8,6 +8,9 @@ import (
 	"github.com/FrancescoLuzzi/AQuickQuestion/app/auth"
 	"github.com/FrancescoLuzzi/AQuickQuestion/app/interfaces"
 	"github.com/FrancescoLuzzi/AQuickQuestion/app/middlewares"
+	"github.com/FrancescoLuzzi/AQuickQuestion/app/types"
+	"github.com/FrancescoLuzzi/AQuickQuestion/app/utils"
+	"github.com/FrancescoLuzzi/AQuickQuestion/app/views/landing"
 	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/schema"
 )
@@ -28,19 +31,27 @@ func NewUserHandler(service interfaces.UserService) UserHandler {
 	}
 }
 
-func (h *UserHandler) RegisterRoutes(mux *http.ServeMux, authMiddleware middlewares.Middleware) {
+func (h *UserHandler) RegisterRoutes(mux *http.ServeMux, md middlewares.Middleware) {
 	// admin routes
-	mux.Handle("GET /user/profile", authMiddleware(http.HandlerFunc(h.handleCurrentUserProfile)))
+	mux.Handle("GET /user/profile", md(http.HandlerFunc(h.handleCurrentUserProfile)))
+	mux.Handle("POST /user/profile", md(http.HandlerFunc(h.handleProfileUpdate)))
+	mux.Handle("POST /user/password", md(http.HandlerFunc(h.handlePasswordUpdate)))
+	mux.Handle("GET /profile", md(http.HandlerFunc(h.handleCurrentUserProfileView)))
 }
 
-func (h *UserHandler) handleCurrentUserProfile(w http.ResponseWriter, r *http.Request) {
+func (h *UserHandler) currentUser(w http.ResponseWriter, r *http.Request) (types.User, error) {
 	ctx := r.Context()
 	userId, err := auth.UserFromCtx(ctx)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return types.User{}, err
 	}
-	user, err := h.service.GetById(ctx, &userId)
+	return h.service.GetById(ctx, &userId)
+
+}
+
+func (h *UserHandler) handleCurrentUserProfile(w http.ResponseWriter, r *http.Request) {
+	user, err := h.currentUser(w, r)
 	if err != nil {
 		slog.Info("failed to get user by id", "err", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -48,4 +59,25 @@ func (h *UserHandler) handleCurrentUserProfile(w http.ResponseWriter, r *http.Re
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
+}
+
+func (h *UserHandler) handleCurrentUserProfileView(w http.ResponseWriter, r *http.Request) {
+	user, err := h.currentUser(w, r)
+	if err != nil {
+		slog.Info("failed to get user by id", "err", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	utils.RenderComponentHandler(landing.Profile(&user)).ServeHTTP(w, r)
+}
+
+func (h *UserHandler) handleProfileUpdate(w http.ResponseWriter, r *http.Request) {
+	// read post body
+	// update username, email, ...
+}
+
+func (h *UserHandler) handlePasswordUpdate(w http.ResponseWriter, r *http.Request) {
+	// read post body
+	// check if old password is correct
+	// hash and save new password
 }
